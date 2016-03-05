10632b52cd5a272d690cea6c39eb6791f1e37caf
Ted Dunning <tdunning@apache.org>
2016-03-04 17:49:53 -0800

    Added R code for plotting antenna results

---
+++ src/main/R/plot-test.r
@@ -0,0 +1,13 @@
+### Plots antenna patterns produced by running the test suite
+
+pdf("plot.pdf", width=4, height=4)
+cat(par("mar"))
+par(mar=c(3.1,4.1,4.1,2.1))
+x = read.csv("/Users/tdunning/Apache/cell-sim/beam-1.csv")
+y = read.csv("/Users/tdunning/Apache/cell-sim/beam-2.csv")
+z = read.csv("/Users/tdunning/Apache/cell-sim/beam-3.csv")
+
+plot(py~px,y, type='l', xlim=c(-1,1), ylim=c(-1,1))
+lines(py~px,x,col='green')
+lines(py~px,z,col='red')
+dev.off()
