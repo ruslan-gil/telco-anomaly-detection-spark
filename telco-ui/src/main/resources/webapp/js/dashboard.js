cba04d16e611775f56bb669801d04b76145b41b5
rkolesnik <r.kolesnik@cybervisiontech.com>
2016-04-15 16:02:14 +0300

    Change NaN to 0.00 in dashbord

--- telco-ui/src/main/resources/webapp/js/dashboard.js
+++ telco-ui/src/main/resources/webapp/js/dashboard.js
@@ -75,6 +75,8 @@ function visualizeStatistics() {
      var time = 0;
      var total = 0;
      var sessions = 0;
+     var resultFailure;
+     var resultDuration;
 
      for( var [el, curStats] of statistics.entries()) {
         var lastInfo = curStats[curStats.length-1];
@@ -83,16 +85,25 @@ function visualizeStatistics() {
         time += lastInfo.time;
         total += lastInfo.towerAllInfo;
         sessions += lastInfo.sessions;
+        resultFailure = ((lastInfo.towerFails/lastInfo.towerAllInfo)*100);
+         if (isNaN(resultFailure)) {
+             resultFailure = 0;
+         }
+        resultDuration = (lastInfo.towerDurations/lastInfo.sessions);
+         if (isNaN(resultDuration)) {
+             resultDuration = 0;
+         }
+         
         var info = `<table>
                          <tr>
                              <td rowspan="2">Tower ${lastInfo.towerId}</td>
                              <td>Status: On | Off | Unknown</td>
-                             <td>%of failure: ${((lastInfo.towerFails/lastInfo.towerAllInfo)*100).toFixed(2) || 0}%</td>
+                             <td>%of failure: ${resultFailure.toFixed(2)}%</td>
                          </tr>
                          <tr>
                              <td>Active Sessions: 0</td>
                              <td>Nb of coll per mn/s: ${(lastInfo.sessions/lastInfo.time).toFixed(2) || 0}</td>
-                             <td>Session Duration: ${(lastInfo.towerDurations/lastInfo.sessions).toFixed(2) || 0}</td>
+                             <td>Session Duration: ${resultDuration.toFixed(2)}</td>
                          </tr>
                      </table>`;
         if (document.getElementById(`tower-info${lastInfo.towerId}`) == undefined) {
@@ -125,10 +136,22 @@ function updateInfo(el, info, curStats){
 }
 
 function updateGlobal(fails, duration, time, total, sessions){
+    var avg_duration = duration/sessions;
+    if (isNaN(avg_duration)){
+        avg_duration = 0;
+    }
+    var failures = fails/total;
+    if (isNaN(failures)) {
+        failures = 0;
+    }
+    var avg_calls = sessions/time;
+    if (isNaN(avg_calls)) {
+        avg_calls = 0;
+    }
     var element = document.getElementById("avg_duration");
-    element.innerHTML = (duration/sessions).toFixed(2) || 0;
+    element.innerHTML = avg_duration.toFixed(2);
     element = document.getElementById("failures");
-    element.innerHTML = (fails/total).toFixed(2) || 0;
+    element.innerHTML = failures.toFixed(2);
     element = document.getElementById("avg_calls");
-    element.innerHTML = (sessions/time).toFixed(2) || 0;
+    element.innerHTML = avg_calls.toFixed(2);
 }
